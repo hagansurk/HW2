b13e54c8b7e2562a0709ec3dd30146bc76ba21fc
Hagan <hhsurk@umich.edu>
2018-01-28 20:23:04 -0500

    Finished Part 1

---
+++ SI364W18_HW2_hhsurk.py
@@ -0,0 +1,75 @@
+## SI 364
+## Winter 2018
+## HW 2 - Part 1
+
+## This homework has 3 parts, all of which should be completed inside this file (and a little bit inside the /templates directory).
+
+## Add view functions and any other necessary code to this Flask application code below so that the routes described in the README exist and render the templates they are supposed to (all templates provided are inside the templates/ directory, where they should stay).
+
+## As part of the homework, you may also need to add templates (new .html files) to the templates directory.
+
+#############################
+##### IMPORT STATEMENTS #####
+#############################
+from flask import Flask, request, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, RadioField, ValidationError
+from wtforms.validators import Required
+import requests
+import json
+#####################
+##### APP SETUP #####
+#####################
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hardtoguessstring'
+
+####################
+###### FORMS #######
+####################
+class Artistform(FlaskForm):
+	artist = StringField('Enter Artist Name Here: ', validators = [Required()])
+	submit = SubmitField('Submit')
+
+class ArtistInfo(FlaskForm):
+	tracks = StringField('Track Name: ', validators=[Required()])
+	submit = SubmitField('Submit')
+
+
+
+####################
+###### ROUTES ######
+####################
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    return '<h1>Hello {0}<h1>'.format(name)
+
+@app.route('/artistform')
+def artist_form():
+	artistForm = Artistform()
+	return render_template('artistform.html', form=artistForm)
+
+@app.route('/artistinfo', methods = ['GET'])
+def artist_info():
+	re = requests.get("https://itunes.apple.com/search?", params = {'term':request.args.get('artist')}).json()
+	return render_template('artist_info.html', objects = re['results'])
+
+@app.route('/artistlinks')
+def link_art():
+	return render_template('artist_links.html')
+
+@app.route('/specific/song/<artist_name>', methods = ['POST', 'GET'])
+def spec_art(artist_name):
+	artist = request.args.get(artist_name)
+	re = requests.get("https://itunes.apple.com/search?", params = {'term':artist_name}).json()
+	print(re)
+	return render_template('specific_artist.html', results = re['resultsy'])
+
+if __name__ == '__main__':
+    app.run(use_reloader=True,debug=True)
